10d63e0fddf5d767a1950096630b4f8e9b0842c5
shahriyor <islomovrofe@gmail.com>
2026-01-08 17:08:01 +0500

    accounts

--- api/views.py
+++ api/views.py
@@ -1,20 +1,56 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework import generics
-from .models import Post
+from rest_framework import generics , response, status,permissions
+from .models import *
 from .serializers import *
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
 
+class RegisterView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def login_api_view(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return Response({'error': 'Please provide both username and password.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({'message':'Login successful!'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message':'Invalid credentials!'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        
+        
+        
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def logout_api_view(request):
+    try:
+        logout(request)
+        return Response({'message':'Logout successful!'}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'message':'An error occurred during logout.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
+
 @api_view(['GET','POST'])
+@permission_classes([permissions.AllowAny])
 def product_list_create_api_view(request):
     if request.method == 'GET':
         serializers = ProductSerializer(data = request.data)
         if serializers.is_valid():
             serializers.save()
+            
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'POST':
@@ -24,6 +60,7 @@ def product_list_create_api_view(request):
         return Response(serializers.data, status=status.HTTP_200_OK)
 
 @api_view(['PATCH','PUT'])
+@permission_classes([permissions.AllowAny]) 
 def product_update_api_views(request,pk):
     product = Product.objects.filter(id=pk).first()
     if not product:
@@ -45,6 +82,7 @@ def product_update_api_views(request,pk):
     
 
 @api_view(['DELETE'])
+@permission_classes([permissions.AllowAny])
 def product_delete_api_view(request,pk):
     product = Product.objects.filter(id=pk).first()
     if not product:
